fe02d37e1aacff4ac0c8905490fd75ffbf482b1a
blake <cblakevill@gmail.com>
2022-04-24 06:48:40 +0930

    styling changes

--- src/app/graph/components/graph/graph.component.ts
+++ src/app/graph/components/graph/graph.component.ts
@@ -30,33 +30,57 @@ export class GraphComponent implements OnInit {
       }
     },
     {
-      selector: 'node',
+      selector: '[name]',
       css: {
         'content': 'data(name)',
         'text-valign': 'center',
-        'shape': 'ellipse',
-        'width': 15,
-        'height': 15,
+        'shape': 'rectangle',
+        'width': 10,
+        'height': 10,
+        'background-color': '#28a4fc',
+        'border-color': '#2b76ab',
+        'border-width': 1
       }
     },
     {
-      selector: ':parent',
+      selector: '[parentBg]',
       css: {
         
         'content': 'data(name)',
         "padding-bottom": "0",
         "padding-top": "0",
+        "padding-left": "0",
+        "padding-right": "0",
         'text-valign': 'top',
         'text-halign': 'center',
         'shape': 'roundrectangle',
-        'border-width': 3
+        'border-width': 3,
+        'border-color': '#49de49',
+        'background-color': 'white'
+      }
+    },
+    {
+      selector: '[invisible]',
+      css: {
+        'height': '1',
+        'width': '1',
+        'content': '',
+        'background-color': '#28a4fc'
       }
     },
     {
       selector: 'edge',
       css: {
-        'curve-style': 'bezier',
-        'target-arrow-shape': 'triangle'
+        'curve-style': 'straight',
+        'target-arrow-shape': 'triangle',
+      }
+    },
+    {
+      selector: '[parentOverlay]',
+      css: {
+        'z-compound-depth': 'top',
+        'background-opacity': 0,
+        'border-width': 0
       }
     }
   ];
@@ -91,22 +115,33 @@ export class GraphComponent implements OnInit {
     var j = tt[dd] ;
     var pp = 80;
     g.forEach(s => {
-      x.push({data:{id:s.name, name: s.name, isParent: true}})
+      x.push(({data:{id:s.name+'parentOverlay', name: '', parentOverlay: true}}))
+      x.push({data:{id:s.name, name: s.name, parent: s.name+'parentOverlay', parentBg: true}})
+      x.push({ data: { id: s.name + 'c1', parent: s.name , weight: dd, isParent: false,  invisible: true}, position: { y: h, x: dd } })
+      h+=pp;
       s.inputs?.forEach(i => {
-        x.push({ data: { id: s.name + 'i' + i , parent: s.name , weight: dd, name: i, isParent: false, right: true}, position: { y: h, x: dd } })
+        x.push({ data: { id: s.name + 'i' + i ,   weight: dd, name: i, isParent: false, right: true, parent: s.name+'parentOverlay'}, position: { y: h, x: dd } })
+        x.push({ data: { id: s.name + 'ii' + i , parent: s.name , weight: dd, isParent: false,  invisible: true}, position: { y: h, x: dd } })
         h+=i.length*pp;
       })
-      h+=70;
+      x.push({ data: { id: s.name + 'c2', parent: s.name , weight: dd, isParent: false,  invisible: true}, position: { y: h, x: dd } })
+      //h+=pp;
+      //x.push({ data: { id: s.name + 'c3', parent: s.name , weight: dd, isParent: false,  invisible: true}, position: { y: h, x: dd } })
+      j+=pp;
       s.outputs?.forEach(i => {
-        x.push({ data: { id: s.name + 'o' + i , parent: s.name , weight: dd, name: i, isParent: false, left: true }, position: { y: j, x: dd+50 } })
+        x.push({ data: { id: s.name + 'o' + i ,  weight: dd, name: i, isParent: false, left: true , parent: s.name+'parentOverlay'}, position: { y: j, x: dd+50 } })
+        x.push({ data: { id: s.name + 'oo' + i , parent: s.name , weight: dd, isParent: false,   invisible: true }, position: { y: j, x: dd+50 } })
         j+=i.length*pp;
       })
-      j+=70;
+      //Sx.push({ data: { id: s.name + 'c4', parent: s.name , weight: dd, isParent: false,   invisible: true }, position: { y: j, x: dd+50 } })
+      j+=pp;
+      //j+=70;
+      h=j=Math.max(h,j)
     })
     
     tt[dd] += Math.max(h,j);
     g.forEach(s => {
-      s.deps?.forEach(d => this.nn(s.deps, x, dd-50-100, tt));
+      s.deps?.forEach(d => this.nn(s.deps, x, dd-50-200, tt));
     })
     return x;
   }
